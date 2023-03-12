1117697ab6504c16452ad602f4789174ab45e9c4
Mastagoon <inzilagiway@gmail.com>
2023-03-12 19:48:39 +0300

    bot

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
-use std::env;
+use dotenv::dotenv;
 
 use serenity::async_trait;
 use serenity::framework::standard::macros::{command, group};
 use serenity::framework::standard::{CommandResult, StandardFramework};
 use serenity::model::channel::Message;
+use serenity::model::gateway::Ready;
 use serenity::prelude::*;
+use std::env;
 
 #[group]
 #[commands(ping)]
@@ -13,17 +15,21 @@ struct General;
 struct Handler;
 
 #[async_trait]
-impl EventHandler for Handler {}
+impl EventHandler for Handler {
+    async fn ready(&self, _: Context, ready: Ready) {
+        println!("{}{} is connected!", ready.user.name, ready.user.tag());
+    }
+}
 
 #[tokio::main]
 async fn main() {
+    dotenv().ok();
     let framework = StandardFramework::new()
         .configure(|c| c.prefix("~")) // set the bot's prefix to "~"
         .group(&GENERAL_GROUP);
 
     // Login with a bot token from the environment
     let token = env::var("BOT_TOKEN").expect("token");
-    println!("Token: {}", token);
     let intents = GatewayIntents::non_privileged() | GatewayIntents::MESSAGE_CONTENT;
     let mut client = Client::builder(token, intents)
         .event_handler(Handler)
@@ -39,6 +45,7 @@ async fn main() {
 
 #[command]
 async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
+    println!("We got a ping ! {}", msg.content);
     msg.reply(ctx, "Pong!").await?;
 
     Ok(())
